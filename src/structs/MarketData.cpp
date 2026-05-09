48d05533a563dc3ba95755c8633d52db72e6a710
Lincoln <lo.shunhang@gmail.com>
2026-05-09 21:41:49 +0800

    init market data classes

---
+++ src/structs/MarketData.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Shun Hang Lo on 5/9/26.
+//
+
+#include "MarketData.h"
+
+namespace mkdt {
+} // mkdt
---
+++ src/structs/MarketData.h
@@ -0,0 +1,13 @@
+//
+// Created by Shun Hang Lo on 5/9/26.
+//
+
+#ifndef CSV_PARSER_MARKETDATA_H
+#define CSV_PARSER_MARKETDATA_H
+
+namespace mkdt {
+    class MarketData {
+    };
+} // mkdt
+
+#endif //CSV_PARSER_MARKETDATA_H
